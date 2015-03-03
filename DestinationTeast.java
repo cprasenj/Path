b196cf5b02567953780c4757e42fe5e0c362fd39
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-03-03 11:47:03 +0530

    test added for getPlace

--- DestinationTeast.java
+++ DestinationTeast.java
@@ -5,12 +5,26 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class DestinationTeast {
-	@Test
-	public void place_will_tell_if_a_given_place_is_present_in_destination_or_not() {
-		Destination d = new Destination("Kolkata");
+	private Destination dest(String place) {
+		Destination d = new Destination(place);
 		d.insert("Dubai");
 		d.insert("Bangalore");
+		return d;
+	}
+
+	@Test
+	public void place_will_tell_if_a_given_place_is_present_in_destination_or_not() {
+		Destination d = this.dest("Kolkata");
 		assertTrue(d.place("Bangalore"));
 		assertFalse(d.place("London"));		
-	}	
+	}
+
+	@Test
+	public void getPlace_gives_the_destination_if_the_string_is_given(){
+		Destination d = this.dest("Kolkata");
+		assertEquals(d.getPlace("Kolkata"),"Kolkata");
+		assertEquals(d.getPlace("Patiala"),null);
+	}
+
+
 }
