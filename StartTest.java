7efbc2b5153f747d2e303b4c168fee080633b068
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-03-03 10:57:12 +0530

    Test added for start

---
+++ StartTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.*;
+import java.util.Map;
+import java.util.HashMap;
+
+public class StartTest {
+	@Test
+	public void start_holds_a_place_name_and_returns_if_asked() {
+		Start s = new Start("Kolkata");
+		assertEquals(s.Place(),"Kolkata");
+	}
+	
+}
